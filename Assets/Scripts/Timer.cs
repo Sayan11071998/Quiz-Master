6533a3b1da6eec3d8400b317e2203d40331059eb
Sayan Nandi <sayannandi11071998@gmail.com>
2025-02-22 01:14:17 +0530

    Timer Created

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private float timeToCompleteQuestion = 30f;
+    [SerializeField] private float timeToShowCorrectAnswer = 10f;
+
+    public bool isAnsweringQuestion = false;
+
+    private float timerValue;
+
+    private void Update()
+    {
+        UpdateTimer();
+    }
+
+    private void UpdateTimer()
+    {
+        timerValue -= Time.deltaTime;
+
+        if (isAnsweringQuestion)
+        {
+            if (timerValue <= 0)
+            {
+                isAnsweringQuestion = false;
+                timerValue = timeToShowCorrectAnswer;
+            }
+        }
+        else
+        {
+            if (timerValue <= 0)
+            {
+                isAnsweringQuestion = true;
+                timerValue = timeToCompleteQuestion;
+            }
+        }
+    }
+}
