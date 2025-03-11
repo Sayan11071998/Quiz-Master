b4ce525b40898396978a2ee00195ec2e12cbcb59
Sayan Nandi <sayannandi11071998@gmail.com>
2025-03-11 16:48:23 +0530

    Update Timer.cs

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -5,7 +5,9 @@ public class Timer : MonoBehaviour
     [SerializeField] private float timeToCompleteQuestion = 30f;
     [SerializeField] private float timeToShowCorrectAnswer = 10f;
 
+    public bool loadNextQuestion;
     public bool isAnsweringQuestion = false;
+    public float fillFraction;
 
     private float timerValue;
 
@@ -14,13 +16,22 @@ public class Timer : MonoBehaviour
         UpdateTimer();
     }
 
+    public void CancleTimer()
+    {
+        timerValue = 0;
+    }
+
     private void UpdateTimer()
     {
         timerValue -= Time.deltaTime;
 
         if (isAnsweringQuestion)
         {
-            if (timerValue <= 0)
+            if (timerValue > 0)
+            {
+                fillFraction = timerValue / timeToCompleteQuestion;
+            }
+            else
             {
                 isAnsweringQuestion = false;
                 timerValue = timeToShowCorrectAnswer;
@@ -28,10 +39,15 @@ public class Timer : MonoBehaviour
         }
         else
         {
-            if (timerValue <= 0)
+            if (timerValue > 0)
+            {
+                fillFraction = timerValue / timeToShowCorrectAnswer;
+            }
+            else
             {
                 isAnsweringQuestion = true;
                 timerValue = timeToCompleteQuestion;
+                loadNextQuestion = true;
             }
         }
     }
